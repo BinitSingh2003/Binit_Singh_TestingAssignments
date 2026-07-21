51a719116c0d5532c586974aa3d07d4423ff1254
Binit_Singh <binitsingh221712@gmail.com>
2026-07-22 00:13:37 +0530

    Add files via upload

---
+++ CoforgeAutomationTesting/SeleniumUsingCICD_jenkins_Installation/src/test/java/JenkinsCICD_DemoTest/JenkinsCICD_test.java
@@ -0,0 +1,95 @@
+package JenkinsCICD_DemoTest;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class JenkinsCICD_test {
+	@Test
+	public void ChromeBrowserTesting(){
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://practicetestautomation.com/");
+		driver.manage().window().maximize();//common step
+		
+		String Browsertitle=driver.getTitle();
+		System.out.println(Browsertitle);
+		
+		String ActualValue= Browsertitle;
+		
+		String Url=driver.getCurrentUrl();
+		System.out.println(Url);
+		
+//		String PageSource=driver.getPageSource();
+//		System.out.println(PageSource);
+		
+		String ExpectedValue="Practice Test Automation | Learn Selenium WebDriver";
+		if(ActualValue.equals(ExpectedValue)) {
+			System.out.println("TestCase Passed");
+		}
+		else {
+			System.out.println("TestCase Failed");
+		}
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void FirefoxBrowserTest() {
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("https://selenium.dev/");
+		driver.manage().window().maximize();//common step
+		
+		String Browsertitle=driver.getTitle();
+		System.out.println(Browsertitle);
+		
+		String ActualValue= Browsertitle;
+
+		
+		String Url=driver.getCurrentUrl();
+		System.out.println(Url);
+		
+//		String PageSource=driver.getPageSource();
+//		System.out.println(PageSource);
+		
+		String ExpectedValue="Selenium";
+		if(ActualValue.equals(ExpectedValue)) {
+			System.out.println("TestCase Passed");
+		}
+		else {
+			System.out.println("TestCase Failed");
+		}
+		driver.quit();
+		
+	}
+	@Test
+	public void EdgeBrowserTest() {
+		EdgeDriver driver=new EdgeDriver();
+		driver.get("https://mvnrepository.com/artifact/org.testng/testng/7.12.0");
+		driver.manage().window().maximize();//common step
+		
+		String Browsertitle=driver.getTitle();
+		System.out.println(Browsertitle);
+		
+		String ActualValue= Browsertitle;
+
+		
+		String Url=driver.getCurrentUrl();
+		System.out.println(Url);
+		
+//		String PageSource=driver.getPageSource();
+//		System.out.println(PageSource);
+		
+		String ExpectedValue="Just a moment...";
+		
+		if(ActualValue.equals(ExpectedValue)) {
+			System.out.println("TestCase Passed");
+		}
+		else {
+			System.out.println("TestCase Failed");
+		}
+		
+	  driver.quit();
+		}
+
+}
